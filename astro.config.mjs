fe90cb7b03482ac10a2fa3e2af314ad8811cb1c5
Jaaziah Espinoza <jaazespinoza@gmail.com>
2025-03-07 13:23:24 -0600

    Fix CSS paths for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://lafieradeportiva.com',
 	outDir: './docs',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
