49e7911477338f67f2fcca1c90ce8ba7eda38308
Jaaziah Espinoza <jaazespinoza@gmail.com>
2025-03-07 13:37:50 -0600

    Fix asset paths for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://lafieradeportiva.com',
-	outDir: './docs',
+	outDir: '/docs',
 	base: '',
 	integrations: [mdx(), sitemap()],
 });
