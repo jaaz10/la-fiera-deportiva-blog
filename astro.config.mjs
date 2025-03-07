1f9516e5b9a6da197247b77c8011f8600ed70e0f
Jaaziah Espinoza <jaazespinoza@gmail.com>
2025-03-07 14:34:26 -0600

    Update site with correct base path configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jaaz10.github.io',
+	site: 'https://lafieradeportiva.com',
 	base: '',
 	integrations: [mdx(), sitemap()],
 });
