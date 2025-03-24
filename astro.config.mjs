115e050c9235ffce3c1c1a0acf73e7da97a67dfd
Jaaziah Espinoza <jaazespinoza@gmail.com>
2025-03-24 09:49:40 -0500

    Removed node.js adapter for server side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lafieradeportiva.com',
+  output: 'static', // Set to static for GitHub Pages
   integrations: [mdx(), sitemap()],
-
-  adapter: node({
-    mode: 'standalone',
-  }),
+  // Remove the Node.js adapter completely
 });
